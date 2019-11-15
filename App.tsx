7afe2c65c1c626a6316358b8f06c7f7e8e9193d0
Felix-Suen <felix.suen2000@gmail.com>
2019-11-14 22:39:12 -0500

    front page

--- App.tsx
+++ App.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View>
+        <Text style={styles.description}>Search for houses here</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
   },
 });
