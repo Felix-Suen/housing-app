5490a1c0c106c8ab5848d0390b6c91ab91c9b259
Felix-Suen <felix.suen2000@gmail.com>
2019-11-14 22:58:58 -0500

    searchpage

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SearchPage from './SearchPage';
 
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
-    return (
-      <View>
-        <Text style={styles.description}>Search for houses here</Text>
-      </View>
+    return(
+      <SearchPage></SearchPage>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  description: {
-    fontSize: 18,
-    textAlign: 'center',
-    color: '#656565',
-    marginTop: 65,
-  },
-});
+
